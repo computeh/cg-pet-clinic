75f9c7f39cd9c4f6221d0d30bd8357a36f5e9586
Caleb Ghirmai <calebyg15@gmail.com>
2021-01-19 18:12:20 -0800

    Created initial project and manually added JUnit maven dependency

---
+++ src/main/java/guru/springframework/cgpetclinic/CgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.cgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgPetClinicApplication.class, args);
+	}
+
+}
