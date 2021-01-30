9253df5f899d3dbf0556c256ffb643e983ca707b
Caleb Ghirmai <calebyg15@gmail.com>
2021-01-29 19:09:03 -0800

    Added Owners List and updated controller. Closes #16.

--- pet-clinic-web/src/main/java/guru/springframework/cgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/cgpetclinic/controllers/OwnerController.java
@@ -1,13 +1,26 @@
 package guru.springframework.cgpetclinic.controllers;
 
+import guru.springframework.cgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    // No need for @Autowired
+    // This is a constructor-type Spring Implementation
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners", "/owners/ownerindex", "/owners/ownerindex.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/ownerindex";
     }
 }
