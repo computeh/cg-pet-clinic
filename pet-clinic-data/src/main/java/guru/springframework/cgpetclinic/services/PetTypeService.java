1d75cd80880f8ab7f470ec422401028136733adb
Caleb Ghirmai <calebyg15@gmail.com>
2021-02-06 19:25:39 -0800

    Added PetType Map Service Impl. Closes #29.

---
+++ pet-clinic-data/src/main/java/guru/springframework/cgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.cgpetclinic.services;
+
+import guru.springframework.cgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
