fac99a5e7b9678c1ff0b3451fc30f7f8028a2b8c
Caleb Ghirmai <calebyg15@gmail.com>
2021-02-06 19:38:31 -0800

    Added PetTypes to BootStrap DataLoader class. Closes #32.

--- pet-clinic-web/src/main/java/guru/springframework/cgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/cgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package guru.springframework.cgpetclinic.bootstrap;
 
 import guru.springframework.cgpetclinic.model.Owner;
+import guru.springframework.cgpetclinic.model.PetType;
 import guru.springframework.cgpetclinic.model.Veterinary;
 import guru.springframework.cgpetclinic.services.OwnerService;
+import guru.springframework.cgpetclinic.services.PetTypeService;
 import guru.springframework.cgpetclinic.services.VeterinaryService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,26 +14,36 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VeterinaryService veterinaryService;
+    private final PetTypeService petTypeService;
 
     // Spring wires data into a Spring context
     // @autowired is not needed if we are using a constructor configuration
-    public DataLoader(OwnerService ownerService, VeterinaryService veterinaryService) {
+
+
+    public DataLoader(OwnerService ownerService, VeterinaryService veterinaryService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.veterinaryService = veterinaryService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType saveDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType saveCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
-        //owner1.setId(1L);
         owner1.setFirstName("Caleb");
-        owner1.setLastName("Ghir");
+        owner1.setLastName("G");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-       // owner2.setId(2L);
         owner2.setFirstName("Michael");
         owner2.setLastName("Weston");
 
@@ -40,14 +52,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Veterinary vet1 = new Veterinary();
-       // vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         veterinaryService.save(vet1);
 
         Veterinary vet2 = new Veterinary();
-       // vet2.setId(2L);
         vet2.setFirstName("George");
         vet2.setLastName("Watkins");
 
