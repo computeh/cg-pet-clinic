ceb233403f4b055216322de5254c249619dbff6f
Caleb Ghirmai <calebyg15@gmail.com>
2021-02-09 20:37:59 -0800

    Added Specialties Map Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/guru/springframework/cgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package guru.springframework.cgpetclinic.services;
+
+import guru.springframework.cgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
