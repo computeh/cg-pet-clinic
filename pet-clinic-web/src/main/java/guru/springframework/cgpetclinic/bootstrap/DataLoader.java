e8ed28354556161123dfd10f5341526b836a3f8d
Caleb Ghirmai <calebyg15@gmail.com>
2021-04-02 07:44:21 -0700

    Upgrade to JUnit 5 Closes #4

--- pet-clinic-web/src/main/java/guru/springframework/cgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/cgpetclinic/bootstrap/DataLoader.java
@@ -76,6 +76,8 @@ public class DataLoader implements CommandLineRunner {
 
         ownerService.save(owner1);
 
+
+
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
